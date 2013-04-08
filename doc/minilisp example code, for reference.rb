e892b41fb7decc6005f2f55431b9c39d4f805ba6
Rory O’Kane <rory@roryokane.com>
2013-04-08 00:57:34 -0400

    copied expected output to minilisp example code

--- doc/minilisp example code, for reference.rb
+++ doc/minilisp example code, for reference.rb
@@ -14,3 +14,10 @@ pp transform.do(result) if result
 
 # Thereby reducing it to the earlier problem: 
 # http://github.com/kschiess/toylisp
+
+# should output (minus ‘#’):
+#[:define,
+# :test,
+# [:lambda,
+#  [],
+#  [:begin, [:display, "something"@54], [:display, 1], [:display, 3.08]]]]
