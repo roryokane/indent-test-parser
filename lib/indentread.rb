50c8b54c7596f0931ae3b8e3965c2d8e5d9446c7
Rory O’Kane <rory@roryokane.com>
2013-04-08 22:09:37 -0400

    add TODOs for editing copy of mini-Lisp, not subclassing

--- lib/indentread.rb
+++ lib/indentread.rb
@@ -16,6 +16,8 @@ module IndentRead
 		# write the indent-handling additions
 	end
 	
+	# TODO I think to stop the MiniLisp parser from gobbling up all of the whitespace, I will have to copy and edit it, not just subclass and add to it.
+	# TODO After getting it basically working, copy prevous code that was removed that is still applicable.
 	class Parser < MiniLisp::Parser
 		# write the indent-handling additions
 	end
