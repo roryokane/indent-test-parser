64a09b1e1f0f74e2b5a88f07ac61702a4cf844d0
Rory O’Kane <rory@roryokane.com>
2013-04-08 22:52:03 -0400

    copy mini-Lisp example into my code
    code reformatted in my style

--- lib/indentread.rb
+++ lib/indentread.rb
@@ -12,14 +12,65 @@ module IndentRead
 		end
 	end
 	
-	class Transform < MiniLisp::Transform
-		# write the indent-handling additions
+	class Transform < Parslet::Transform
+		# TODO write the indent-handling additions
+		
+		rule(:identifier => simple(:ident)) { ident.to_sym }
+		
+		rule(:string => simple(:str)) { str }
+		
+		rule(:integer => simple(:int)) { Integer(int) }
+		
+		rule(:float=>{:integer=> simple(:a), :e=> simple(:b)}) do
+			Float(a + b)
+		end
+		
+		rule(:exp => subtree(:exp)) { exp }
 	end
 	
-	# TODO I think to stop the MiniLisp parser from gobbling up all of the whitespace, I will have to copy and edit it, not just subclass and add to it.
-	# TODO After getting it basically working, copy prevous code that was removed that is still applicable.
-	class Parser < MiniLisp::Parser
-		# write the indent-handling additions
+	# TODO After getting this copy of MiniLisp basically working, paste back previous code that was removed that is still applicable.
+	class Parser < Parslet::Parser
+		# TODO write the indent-handling additions
+		
+		root :expression
+		rule(:expression) {
+			space? >> str('(') >> space? >> body >> str(')') >> space?
+		}
+		
+		rule(:body) {
+			(expression | identifier | float | integer | string).repeat.as(:exp)
+		}
+		
+		rule(:space) {
+			match('\s').repeat(1)
+		}
+		rule(:space?) {
+			space.maybe
+		}
+		
+		rule(:identifier) { 
+			(match('[a-zA-Z=*]') >> match('[a-zA-Z=*_]').repeat).as(:identifier) >> space?
+		}
+		
+		rule(:float) { 
+			(
+				integer >> (
+					str('.') >> match('[0-9]').repeat(1) |
+					str('e') >> match('[0-9]').repeat(1)
+				).as(:e)
+			).as(:float) >> space?
+		}
+		
+		rule(:integer) {
+			((str('+') | str('-')).maybe >> match("[0-9]").repeat(1)).as(:integer) >> space?
+		}
+		
+		rule(:string) {
+			str('"') >> (
+				str('\\') >> any |
+				str('"').absent? >> any 
+			).repeat.as(:string) >> str('"') >> space?
+		}
 	end
 end
 
