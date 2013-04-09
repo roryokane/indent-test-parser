a50b73cfe8cd937aad30b29e5ebbe513193a67ce
Rory O’Kane <rory@roryokane.com>
2013-04-09 13:31:59 -0400

    start writing indent-parsing

--- lib/indentread.rb
+++ lib/indentread.rb
@@ -30,26 +30,51 @@ module IndentRead
 	
 	# TODO After getting this copy of MiniLisp basically working, paste back previous code that was removed that is still applicable.
 	class Parser < Parslet::Parser
-		# TODO write the indent-handling additions
-		
-		root :expression
+		root(:expression)
 		rule(:expression) {
-			space? >> str('(') >> space? >> body >> str(')') >> space?
+			s_expression | i_expression
 		}
 		
-		rule(:body) {
-			(expression | identifier | float | integer | string).repeat.as(:exp)
+		rule(:i_expression) {
+			i_body >> inline_space? >> newline
 		}
 		
-		rule(:space) {
+		rule(:i_body) {
+			# FIXME right now, there is infinite left recursion
+			# but before rushing into fixing it, finish making the expression grammars actually plausible
+			(i_expression >> inline_space?) >> ((s_expression | value) >> inline_space?).repeat.as(:exp)
+		}
+		
+		rule(:newline) {
+			str("\n")
+		}
+		rule(:inline_space) {
+			match[' '].repeat(1)
+		}
+		rule(:inline_space?) {
+			inline_space.maybe
+		}
+		rule(:any_space) {
 			match('\s').repeat(1)
 		}
-		rule(:space?) {
-			space.maybe
+		rule(:any_space?) {
+			any_space.maybe
+		}
+		
+		rule(:s_expression) {
+			str('(') >> any_space? >> s_body >> str(')')
+		}
+		
+		rule(:s_body) {
+			((s_expression | value) >> any_space?).repeat.as(:exp)
+		}
+		
+		rule(:value) {
+			identifier | float | integer | string
 		}
 		
 		rule(:identifier) { 
-			(match('[a-zA-Z=*]') >> match('[a-zA-Z=*_]').repeat).as(:identifier) >> space?
+			(match('[a-zA-Z=*]') >> match('[a-zA-Z=*_]').repeat).as(:identifier)
 		}
 		
 		rule(:float) { 
@@ -58,18 +83,18 @@ module IndentRead
 					str('.') >> match('[0-9]').repeat(1) |
 					str('e') >> match('[0-9]').repeat(1)
 				).as(:e)
-			).as(:float) >> space?
+			).as(:float)
 		}
 		
 		rule(:integer) {
-			((str('+') | str('-')).maybe >> match("[0-9]").repeat(1)).as(:integer) >> space?
+			((str('+') | str('-')).maybe >> match("[0-9]").repeat(1)).as(:integer)
 		}
 		
 		rule(:string) {
 			str('"') >> (
 				str('\\') >> any |
 				str('"').absent? >> any 
-			).repeat.as(:string) >> str('"') >> space?
+			).repeat.as(:string) >> str('"')
 		}
 	end
 end
